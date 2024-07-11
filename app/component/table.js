a45394cd8467016a9bad46e825885b8c60aa013d
Majd Zarai <majdzari2@gmail.com>
2024-07-11 13:44:27 +0100

    creation du tableau et du boutton ajouter

---
+++ app/component/table.js
@@ -0,0 +1,26 @@
+export default function Table(){
+    return(
+        <table className="min-w-full table-auto">
+            <thead>
+                <tr className="bg-gray-400">
+                    <th className="px-16 py-2">
+                        <span className="text-gray-200"> Name</span>
+                    </th>
+                    <th className="px-16 py-2">
+                        <span className="text-gray-200"> Section</span>
+                    </th>
+                    <th className="px-16 py-2">
+                        <span className="text-gray-200"> Price</span>
+                    </th>
+                    <th className="px-16 py-2">
+                        <span className="text-gray-200"> Description</span>
+                    </th>
+                </tr>
+
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    )
+}
